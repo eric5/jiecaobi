80956fd75993fec17926e9c71be4ec4471a793ec
Eric Wu <ericwu1989@gmail.com>
2015-10-28 23:19:19 +0800

    Show how many JCB left this user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 
+  MONTHLY_JCB_NUMBER = 10
+
   def index
     @reputations = Reputation.order(created_at: :desc).page(params[:page])
+    @jcb_left_this_month = MONTHLY_JCB_NUMBER - Reputation.monthly_limitation(current_user).length
   end
 end
