9c51571d16cc294ef01cfabf3198d2ce39941989
Eric Wu <ericwu1989@gmail.com>
2015-11-11 11:10:06 +0800

    Add stats page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
   def index
     @reputations = Reputation.order(created_at: :desc).page(params[:page])
     @jcb_left_this_month = MONTHLY_JCB_NUMBER - Reputation.monthly_limitation(current_user).length
+  end
+
+  def stats
     @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'register', to: 'users#new'
   get 'sign_in', to: 'sessions#new', as: 'sign_in'
   get 'sign_out', to: 'sessions#destroy'
+  get 'stats', to: 'home#stats'
 
   resources :users
   resources :sessions, only: [:create]
